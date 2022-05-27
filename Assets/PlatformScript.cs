1fbd3d919d883c62c34f36f4fdb7b331026fdf79
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-05-27 09:42:56 -0300

    Initial commit

---
+++ Assets/PlatformScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    public float xVelocidad, LimiteDerecha, LImiteIzquierda;
+    bool Derecha;
+    public GameObject Cilindro, Esfera, baseizquierda, basederecha;
+
+    void Start()
+    {
+        Derecha = true;
+    }
+
+    void Update()
+    {
+        if (Derecha)
+        {
+            transform.position += new Vector3(xVelocidad, 0, 0);
+        }
+
+        else
+        {
+            transform.position -= new Vector3(xVelocidad, 0, 0);
+        }
+
+        if (transform.position.x >= LimiteDerecha)
+        {
+            Derecha = false;
+            GameObject clon = Instantiate(Cilindro);
+            clon.transform.position = basederecha.transform.position + new Vector3(0, 1, 0);
+        }
+
+        else if (transform.position.x <= LImiteIzquierda)
+        {
+            Derecha = true;
+            GameObject clon = Instantiate(Esfera);
+            clon.transform.position = baseizquierda.transform.position + new Vector3(0, 1, 0); 
+            
+        }
+    }
+}
